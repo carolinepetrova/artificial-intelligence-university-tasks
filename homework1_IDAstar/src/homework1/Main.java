2d14d1cacd03e44be9cd889d42fe084dd6ada41c
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-10-26 11:31:05 +0300

    Add another test case TODO: Does not generate correct moves!

--- homework1_IDAstar/src/homework1/Main.java
+++ homework1_IDAstar/src/homework1/Main.java
@@ -3,6 +3,13 @@ package homework1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("homework1");
+        /*
+        The algorithm is tested with the following grid:
+        1 2 3
+        4 5 6
+        0 7 8
+        And returns two moves 'LEFT, LEFT' which is the correct way to solve the puzzle given this state.
+        This can be seen in Tests.java -  testIDAStarSearchGeneratesCorrectEndNode() method.
+        */
     }
 }
--- homework1_IDAstar/src/homework1/Tests.java
+++ homework1_IDAstar/src/homework1/Tests.java
@@ -12,6 +12,14 @@ import static org.testng.Assert.assertTrue;
 
 public class Tests {
 
+    /**
+     * Creates
+     * 1 2 3
+     * 4 5 6
+     * 0 7 8
+     *
+     * Expected solution is LEFT LEFT
+     */
     public static Node createTestInitialNode1() {
         List<List<Tile>> grid = new ArrayList<>();
 
@@ -47,6 +55,49 @@ public class Tests {
         return new Node(grid, 3);
     }
 
+    /**
+     * Creates
+     * 1 2 3
+     * 0 4 6
+     * 7 5 8
+     *
+     * Expected solution is LEFT UP LEFT
+     */
+    public static Node createTestInitialNode2() {
+        List<List<Tile>> grid = new ArrayList<>();
+
+        Tile tile1 = new Tile(new Position(0, 0), 1, 1);
+        Tile tile2 = new Tile(new Position(0, 1), 2, 2);
+        Tile tile3 = new Tile(new Position(0, 2), 3, 3);
+        Tile tile4 = new Tile(new Position(1, 0), 0, 4);
+        Tile tile5 = new Tile(new Position(1, 1), 4, 5);
+        Tile tile6 = new Tile(new Position(1, 2), 6, 6);
+        Tile tile7 = new Tile(new Position(2, 0), 7, 7);
+        Tile tile8 = new Tile(new Position(2, 1), 5, 8);
+        Tile tile9 = new Tile(new Position(2, 2), 8, 0);
+
+        List<Tile> row1 = new ArrayList<>();
+        row1.add(tile1);
+        row1.add(tile2);
+        row1.add(tile3);
+
+        List<Tile> row2 = new ArrayList<>();
+        row2.add(tile4);
+        row2.add(tile5);
+        row2.add(tile6);
+
+        List<Tile> row3 = new ArrayList<>();
+        row3.add(tile7);
+        row3.add(tile8);
+        row3.add(tile9);
+
+        grid.add(row1);
+        grid.add(row2);
+        grid.add(row3);
+
+        return new Node(grid, 3);
+    }
+
     @Test
     public void testManhattanDistanceIsCalculatedCorrectly() {
         Tile tileA = new Tile(2, 0);
@@ -55,11 +106,25 @@ public class Tests {
     }
 
     @Test
-    public void testIDAStarSearchGeneratesCorrectEndNode() {
+    public void testAlgorithmGeneratesCorrectMoves1() {
         IDAStarSearch algorithm = new IDAStarSearch(Tests.createTestInitialNode1());
 
         List<Move> expectedMoves = new ArrayList<>();
         expectedMoves.add(Move.LEFT);
+        expectedMoves.add(Move.LEFT);
+
+        Optional<List<Move>> optMovesToSolution = algorithm.findMovesToSolution();
+        assertTrue(optMovesToSolution.isPresent());
+        assertEquals(expectedMoves, optMovesToSolution.get());
+    }
+
+    @Test
+    public void testAlgorithmGeneratesCorrectMoves2() {
+        IDAStarSearch algorithm = new IDAStarSearch(Tests.createTestInitialNode2());
+
+        List<Move> expectedMoves = new ArrayList<>();
+        expectedMoves.add(Move.LEFT);
+        expectedMoves.add(Move.UP);
         expectedMoves.add(Move.LEFT);
 
         Optional<List<Move>> optMovesToSolution = algorithm.findMovesToSolution();
