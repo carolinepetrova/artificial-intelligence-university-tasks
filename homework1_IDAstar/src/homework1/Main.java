92d40d33e76463b56bf8c26c63b2eb19dfba120c
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-10-26 12:31:41 +0300

    main

--- homework1_IDAstar/src/homework1/Main.java
+++ homework1_IDAstar/src/homework1/Main.java
@@ -3,13 +3,7 @@ package homework1;
 public class Main {
 
     public static void main(String[] args) {
-        /*
-        The algorithm is tested with the following grid:
-        1 2 3
-        4 5 6
-        0 7 8
-        And returns two moves 'LEFT, LEFT' which is the correct way to solve the puzzle given this state.
-        This can be seen in Tests.java -  testIDAStarSearchGeneratesCorrectEndNode() method.
-        */
+        // The algorithm tests can be found in Tests.java
+        System.out.println("Homework1");
     }
 }
