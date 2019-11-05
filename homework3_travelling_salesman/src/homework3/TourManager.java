13719996f24b02e65fed6a69a1ffda04251df925
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-11-05 21:17:19 +0200

    Add Tour and TourManager

---
+++ homework3_travelling_salesman/src/homework3/TourManager.java
@@ -0,0 +1,25 @@
+package homework3;
+
+import java.util.ArrayList;
+
+// Holds the cities of a tour
+public class TourManager {
+
+    // Holds our cities
+    private static ArrayList destinationCities = new ArrayList<City>();
+
+    // Adds a destination city
+    public static void addCity(City city) {
+        destinationCities.add(city);
+    }
+
+    // Get a city
+    public static City getCity(int index) {
+        return (City) destinationCities.get(index);
+    }
+
+    // Get the number of destination cities
+    public static int numberOfCities() {
+        return destinationCities.size();
+    }
+}
