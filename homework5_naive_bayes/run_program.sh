e8b50b19000ca770ef28b8997827c40c00aae376
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-12-24 13:01:01 +0200

    Modify script for program running to run unit tests first

--- homework5_naive_bayes/run_program.sh
+++ homework5_naive_bayes/run_program.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 
+g++ -std=c++17 -ggdb test.cpp -o test && ./test
+
+if [[ $? -ne 0 ]]; then
+    echo "Unit tests failed! Will not run program!";
+    exit 1
+fi
+
 g++ -std=c++17 -ggdb main.cpp -o main && ./main > results.txt
 
