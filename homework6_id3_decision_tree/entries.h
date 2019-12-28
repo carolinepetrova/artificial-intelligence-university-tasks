eda0cdbecbf9190129de831b5f09ac9ce8edb5a0
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-12-28 18:04:43 +0200

    Unit test fails; core dump; at least some progress...

--- homework6_id3_decision_tree/entries.h
+++ homework6_id3_decision_tree/entries.h
@@ -17,66 +17,9 @@ class Entries {
  private:
   vector<vector<string>> data;
 
-  /**
-   * @brief get all possible classes from the data
-   *
-   * In each row of the csv formatted data, represented as vector<string>, the
-   * 0th element is expected to correspond to the class
-   */
-  unordered_set<string> getClasses() const {
-    unordered_set<string> result;
-    for_each(data.begin(), data.end(),
-             [&result](const auto& entry) { result.insert(entry[0]); });
-    return result;
-  }
-
-  int countEntriesByAttributeValue(int attributeId,
-                                   string attributeValue) const {
-    int result = 0;
-    for_each(data.begin(), data.end(), [&result](const auto& entry) {
-      if (entry[attributeId] == attributeValue) result++;
-    });
-    return result;
-  }
-
-  unordered_set<string> getAllPossibleAttributeValues(int attributeId) const {
-    unordered_set<string> result;
-    for_each(data.begin(), data.end(), [&result](const auto& entry) {
-      result.insert(entry[attributeId]);
-    });
-    return result;
-  }
-
-  /**
-   * @brief calculate E(A=x), e.g. E(Outlook=sunny)
-   */
-  Entropy calculateAttributeEntropy(int attributeId,
-                                    string attributeValue) const {
-    // 1. Create dataset only from entries which have the given attribute value
-    // 2. Map each class to number of entries which belong to the class in the dataset
-    // 3. Call utility function to do the math
-
-    // ------------------------
-
-    unordered_map<Class, EntriesCount> classToCountMap;
-
-    // initialize map with zeros
-    for (const auto& c : getClasses()) {
-      classToCountMap[c] = 0;
-    }
-
-    // fill the map
-    for_each(data.begin(), data.end(), [&classToCountMap](const auto& entry) {
-      if (entry[attributeId] == attributeValue) {
-        const string& currentClass = entry[0];
-        classToCountMap[currentClass]++;
-      }
-    });
-
-    return calculateEntropy(classToCountMap);
-  }
-
  public:
+  Entries(const vector<vector<string>>& entries) : data{entries} {}
+
   bool areAllEntriesWithSameClass() const { return getClasses().size() == 1; }
   bool isEmpty() const { return data.empty(); }
 
@@ -89,7 +32,8 @@ class Entries {
     int lowestAverageInformationEntropy = 10000;
 
     // start from 1, because the 0th index belongs to the class
-    for (int attributeId = 1; attributeId < data.size(); attributeId++) {
+    for (AttributeId attributeId = 1; attributeId < data.size();
+         attributeId++) {
       unordered_map<string, pair<EntriesCount, Entropy>>
           attributeValueToEntriesCountAndEntropyPairMap;
 
@@ -117,6 +61,73 @@ class Entries {
         }
       }
     }
+
+    return attributeWithHighestInformationGain;
+  }
+
+ private:
+  /**
+   * Helper methods
+   */
+
+  /**
+   * @brief get all possible classes from the data
+   *
+   * In each row of the csv formatted data, represented as vector<string>, the
+   * 0th element is expected to correspond to the class
+   */
+  unordered_set<string> getClasses() const {
+    unordered_set<string> result;
+    for_each(data.begin(), data.end(),
+             [&result](const auto& entry) { result.insert(entry[0]); });
+    return result;
+  }
+
+  int countEntriesByAttributeValue(int attributeId,
+                                   string attributeValue) const {
+    int result = 0;
+    for_each(data.begin(), data.end(), [&](const vector<string>& entry) {
+      if (entry[attributeId] == attributeValue) result++;
+    });
+    return result;
+  }
+
+  unordered_set<string> getAllPossibleAttributeValues(int attributeId) const {
+    unordered_set<string> result;
+    for_each(data.begin(), data.end(), [&](const vector<string>& entry) {
+      result.insert(entry[attributeId]);
+    });
+    return result;
+  }
+
+  /**
+   * @brief calculate E(A=x), e.g. E(Outlook=sunny)
+   */
+  Entropy calculateAttributeEntropy(int attributeId,
+                                    string attributeValue) const {
+    // 1. Create dataset only from entries which have the given attribute value
+    // 2. Map each class to number of entries which belong to the class in the
+    // dataset
+    // 3. Call utility function to do the math
+
+    // ------------------------
+
+    unordered_map<Class, EntriesCount> classToCountMap;
+
+    // initialize map with zeros
+    for (const auto& c : getClasses()) {
+      classToCountMap[c] = 0;
+    }
+
+    // fill the map
+    for_each(data.begin(), data.end(), [&](const vector<string>& entry) {
+      if (entry[attributeId] == attributeValue) {
+        const string& currentClass = entry[0];
+        classToCountMap[currentClass]++;
+      }
+    });
+
+    return calculateEntropy(classToCountMap);
   }
 };
 
--- homework6_id3_decision_tree/test.cpp
+++ homework6_id3_decision_tree/test.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 
 #include "catch.hpp"
+#include "entries.h"
 #include "utils.h"
 
 bool double_equals(double a, double b, double epsilon = 0.001) {
@@ -15,10 +16,37 @@ TEST_CASE("Entropy is calculated correctly") {
   REQUIRE(id3::calculateEntropy({{"A", 2}}) == 0);
 }
 
-TEST_CASE("Average information entropy is calculated correctly") {
+TEST_CASE("Average information entropy should be calculated correctly") {
   REQUIRE(double_equals(
       id3::calculateAverageInformationEntropy(
           14,
           {{"value1", {5, 0.971}}, {"value2", {5, 0.971}}, {"value3", {4, 0}}}),
       0.693));
+}
+
+TEST_CASE(
+    "The attribute with highest information gain should be calculated "
+    "correctly") {
+  const vector<vector<string>> data = {
+      {"No", "Sunny", "Hot", "High", "Weak"},
+      {"No", "Sunny", "Hot", "High", "Strong"},
+      {"Yes", "Overcast", "Hot", "High", "Weak"},
+      {"Yes", "Rainy", "Mild", "High", "Weak"},
+      {"Yes", "Rainy", "Cool", "Normal", "Weak"},
+      {"No", "Rainy", "Cool", "Normal", "Strong"},
+      {"Yes", "Overcast", "Cool", "Normal", "Strong"},
+      {"No", "Sunny", "Mild", "High", "Weak"},
+      {"Yes", "Sunny", "Cool", "Normal", "Weak"},
+      {"Yes", "Rainy", "Mild", "Normal", "Weak"},
+      {"Yes", "Sunny", "Mild", "Normal", "Strong"},
+      {"Yes", "Overcast", "Mild", "High", "Strong"},
+      {"Yes", "Overcast", "Hot", "Normal", "Weak"},
+      {"No", "Rainy", "Mild", "High", "Strong"},
+  };
+
+  id3::Entries entries{data};
+
+  CAPTURE(entries.getAttributeWithHighestInformationGain() == 1);
+  REQUIRE(entries.getAttributeWithHighestInformationGain() ==
+          1);  // entry[1] is the Outlook attribute
 }
