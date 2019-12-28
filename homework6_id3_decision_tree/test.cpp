34ed879c117bb6628faad099086228eeaed032db
Stiliyan Goranov <stiliqn.goranov@gmail.com>
2019-12-28 19:15:34 +0200

    refactor

--- homework6_id3_decision_tree/test.cpp
+++ homework6_id3_decision_tree/test.cpp
@@ -9,6 +9,25 @@ bool double_equals(double a, double b, double epsilon = 0.001) {
   return std::abs(a - b) < epsilon;
 }
 
+vector<vector<string>> generateTestData() {
+  return {
+      {"No", "Sunny", "Hot", "High", "Weak"},
+      {"No", "Sunny", "Hot", "High", "Strong"},
+      {"Yes", "Overcast", "Hot", "High", "Weak"},
+      {"Yes", "Rainy", "Mild", "High", "Weak"},
+      {"Yes", "Rainy", "Cool", "Normal", "Weak"},
+      {"No", "Rainy", "Cool", "Normal", "Strong"},
+      {"Yes", "Overcast", "Cool", "Normal", "Strong"},
+      {"No", "Sunny", "Mild", "High", "Weak"},
+      {"Yes", "Sunny", "Cool", "Normal", "Weak"},
+      {"Yes", "Rainy", "Mild", "Normal", "Weak"},
+      {"Yes", "Sunny", "Mild", "Normal", "Strong"},
+      {"Yes", "Overcast", "Mild", "High", "Strong"},
+      {"Yes", "Overcast", "Hot", "Normal", "Weak"},
+      {"No", "Rainy", "Mild", "High", "Strong"},
+  };
+}
+
 TEST_CASE("Entropy is calculated correctly") {
   REQUIRE(id3::calculateEntropy({{"A", 4}, {"B", 2}, {"C", 1}, {"D", 1}}) ==
           1.75);
@@ -27,22 +46,7 @@ TEST_CASE("Average information entropy should be calculated correctly") {
 TEST_CASE(
     "The attribute with highest information gain should be calculated "
     "correctly") {
-  const vector<vector<string>> data = {
-      {"No", "Sunny", "Hot", "High", "Weak"},
-      {"No", "Sunny", "Hot", "High", "Strong"},
-      {"Yes", "Overcast", "Hot", "High", "Weak"},
-      {"Yes", "Rainy", "Mild", "High", "Weak"},
-      {"Yes", "Rainy", "Cool", "Normal", "Weak"},
-      {"No", "Rainy", "Cool", "Normal", "Strong"},
-      {"Yes", "Overcast", "Cool", "Normal", "Strong"},
-      {"No", "Sunny", "Mild", "High", "Weak"},
-      {"Yes", "Sunny", "Cool", "Normal", "Weak"},
-      {"Yes", "Rainy", "Mild", "Normal", "Weak"},
-      {"Yes", "Sunny", "Mild", "Normal", "Strong"},
-      {"Yes", "Overcast", "Mild", "High", "Strong"},
-      {"Yes", "Overcast", "Hot", "Normal", "Weak"},
-      {"No", "Rainy", "Mild", "High", "Strong"},
-  };
+  const vector<vector<string>> data = generateTestData();
 
   id3::Entries entries{data};
 
@@ -50,6 +54,6 @@ TEST_CASE(
       entries.getAttributeWithHighestInformationGain();
 
   CAPTURE(attributeWithHighestInformationGain);
-  REQUIRE(attributeWithHighestInformationGain == 1);  // entry[1] is the Outlook
+  REQUIRE(attributeWithHighestInformationGain == 1);  // entry[1] is the 'Outlook'
                                                       // attribute
 }
